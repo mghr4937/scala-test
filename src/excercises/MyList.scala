9892877bf9585e2bdfda2bbaaa92aefbd50b6ffe
Matías Hernández <m.hernandez@globant.com>
2020-06-02 16:24:37 -0300

    21. Case Classes

--- src/excercises/MyList.scala
+++ src/excercises/MyList.scala
@@ -16,7 +16,7 @@ abstract class MyList[+A] {
   def ++[B>: A](list: MyList[B]): MyList[B]
 }
 
-object Empty extends MyList[Nothing] {
+case object Empty extends MyList[Nothing] {
 
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
@@ -32,7 +32,7 @@ object Empty extends MyList[Nothing] {
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
-class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   def head: A = h
   def tail: MyList[A] = t
@@ -65,14 +65,14 @@ trait MyTransformer[-A, B]{
 }
 
 
-object ListTest extends App{
-//  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-//
-//  println(list.tail.head)
-//  println(list.add(4).head)
-//  println(list.isEmpty)
-//
-//  println(list.toString)
+object ListTest extends App {
+  //  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  //
+  //  println(list.tail.head)
+  //  println(list.add(4).head)
+  //  println(list.isEmpty)
+  //
+  //  println(list.toString)
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, new Cons(6, Empty)))
   val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
@@ -92,4 +92,9 @@ object ListTest extends App{
   println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
     override def transform(element: Int): MyList[Int] = new Cons(element, new Cons(element + 1, Empty))
   }).toString)
+
+  //case
+  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+
+  println(cloneListOfIntegers == listOfIntegers)
 }
